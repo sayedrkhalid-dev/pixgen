25a410c44bd63c7422a80e02b6f86fb0ef3f8092
Sayed <sayedrkhalid.official@gmail.com>
2026-04-28 20:20:31 +0600

    normalize default ui

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,10 +19,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html
+      suppressHydrationWarning
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
